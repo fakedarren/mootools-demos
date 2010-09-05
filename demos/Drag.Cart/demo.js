9c64d10c7a0bc735aa6e55fe4eb8474a0cd5cc40
Darren Waddell <darren.waddell@gmail.com>
2010-09-05 16:11:45 +0100

    Updating teh awesome cart demo

--- demos/Drag.Cart/demo.js
+++ demos/Drag.Cart/demo.js
@@ -1,39 +1,37 @@
-var drop = $('cart');
-var dropFx = drop.effect('background-color', {wait: false}); // wait is needed so that to toggle the effect,
- 
-$$('.item').each(function(item){
- 
-	item.addEvent('mousedown', function(e) {
-		e = new Event(e).stop();
- 
-		var clone = this.clone()
-			.setStyles(this.getCoordinates()) // this returns an object with left/top/bottom/right, so its perfect
-			.setStyles({'opacity': 0.7, 'position': 'absolute'})
-			.addEvent('emptydrop', function() {
-				this.remove();
-				drop.removeEvents();
+window.addEvent('domready', function(){
+	
+	$$('.item').addEvent('mousedown', function(evt){
+		
+		evt.stop();
+ 		
+		var clone = this.clone().setStyles(this.getCoordinates()).setStyles({
+				'opacity': 0.7,
+				'position': 'absolute'
 			}).inject(document.body);
  
-		drop.addEvents({
-			'drop': function() {
-				drop.removeEvents();
-				clone.remove();
-				item.clone().inject(drop);
-				dropFx.start('7389AE').chain(dropFx.start.pass('ffffff', dropFx));
+ 		var shirt = this;
+
+		var drag = new Drag.Move(clone, {
+		
+			droppables: $('cart'),
+			
+			onDrop: function(dragging, cart){
+			
+				dragging.destroy();
+			
+				if (cart != null){
+					shirt.clone().inject(cart);
+					cart.highlight('#7389AE', '#FFF');
+				}
 			},
-			'over': function() {
-				dropFx.start('98B5C1');
+			onEnter: function(dragging, cart){
+				cart.tween('background-color', '#98B5C1');
 			},
-			'leave': function() {
-				dropFx.start('ffffff');
+			onLeave: function(dragging, cart){
+				cart.tween('background-color', '#FFF');
 			}
 		});
- 
-		var drag = clone.makeDraggable({
-			droppables: [drop]
-		}); // this returns the dragged element
- 
-		drag.start(e); // start the event manual
+		drag.start(evt);
 	});
- 
+	
 });
