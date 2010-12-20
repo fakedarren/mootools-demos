979b7fea2c08475d35f9e7c8322997a39fab9a31
fakedarren <darren.waddell@gmail.com>
2010-12-20 21:19:02 +0000

    Simple Accordion demo

--- demos/Accordion/demo.js
+++ demos/Accordion/demo.js
@@ -1,13 +1,2 @@
-window.addEvent('domready', function() {
-	
-	var myAccordion = new Accordion($('accordion'), 'h3.toggler', 'div.element', {
-		opacity: false,
-		onActive: function(toggler, element){
-			toggler.setStyle('color', '#41464D');
-		},
-		onBackground: function(toggler, element){
-			toggler.setStyle('color', '#528CE0');
-		}
-	});
 
-});
+new Fx.Accordion($('accordion'), 'h2', '.content');
