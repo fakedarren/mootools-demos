9d458e8c9d7165a1f5ffbfa47c0632aaba3862a1
fakedarren <darren.waddell@gmail.com>
2010-12-21 13:27:15 +0000

    Simpler Effects demo

--- demos/Effects/demo.js
+++ demos/Effects/demo.js
@@ -1,82 +1,43 @@
+
 window.addEvent('domready', function() {
-	var el = $('myElement');
+
+
+	$$('.simple-fx LI').addEvent('click', function(){
 	
-	// MooTools is able to handle effects without the use of a wrapper,
-	// so you are able to do effects with just one easy line.
-	
-	//FIRST EXAMPLE
-	
-	// There are different ways to add a fading opacity effect to an element click
-	
-	// Short version
-	$('fadeOpacity').addEvent('click', el.fade.bind(el, [0]));
-	
-	// Long version
-	$('tweenOpacity').addEvent('click', function(e) {
-		// You often will need to stop propagation of the event
-		e.stop();
-		el.fade(1);
-	});
-	
-	$('tweenOpacity1').addEvent('click', function(e) {
-		e.stop();
-		el.fade(0.3);
+		var property = $(this).get('data-property');
+		var to = $(this).get('data-value');
+		
+		$('element').tween(property, to);
+		
 	});
 	
 	
-	//SECOND EXAMPLE
-	var otherEl = $('myOtherElement');
-	
-	$('heightEffect').addEvent('click', function(){
-		otherEl.tween('height', 30);
-		return false; // alternative syntax to stop the event
-	});
-	
-	// We can also create an Fx.Tween instance and use a wrapper variable
-	
-	var myEffect = new Fx.Tween(otherEl);
-	$('colorEffect').addEvent('click', function(e) {
-		e.stop();
-		// We change the background-color of the element
-		myEffect.start('background-color', '#E6EFC2');
-	});
-	
-	$('borderEffect').addEvent('click', function(e) {
-		e.stop();
-		otherEl.tween('border', '10px dashed #C6D880');
-	});
-	
-	$('resetEffect').addEvent('click', function(e) {
-		e.stop();
-		otherEl.erase('style');
-	});
-	
-	
-	//THIRD EXAMPLE
-	
-	var anotherEl = $('anotherElement');
-	
-	// Again we are able to create a morph instance
-	var morph = new Fx.Morph('anotherElement');
-	
-	$('morphEffect').addEvent('click', function(e) {
-		e.stop();
-		morph.start({
-			width: '200px',
-			color: '#C6D880'
+	$('morph-1').addEvent('click', function(){
+		$('element').morph({
+			'background-color': '#F9F9F9',
+			'color': '#C6D880',
+			'width': '200px'
 		});
 	});
 	
-	// Or we just use Element.morph
-	$('CSSmorphEffect').addEvent('click', function(e) {
-		e.stop();
-		// Changes the element's style to .myClass defined in the CSS
-		anotherEl.morph('.myClass');
+	$('morph-2').addEvent('click', function(){
+		$('element').morph({
+			'background-color': '#B86364',
+			'border': '1px solid red',
+			'height': '45px',
+			'opacity': '0.6',
+			'width': '100px'
+		});
 	});
 	
-	$('resetEffect1').addEvent('click', function(e) {
-		e.stop();
-		// You need the same selector defined in the CSS-File
-		anotherEl.morph('div.demoElement');
+	$('morph-3').addEvent('click', function(){
+		$('element').morph('.myClass')
 	});
+	
+	
+	$('reset').addEvent('click', function(){
+		$('element').erase('style');
+	});
+	
+
 });
