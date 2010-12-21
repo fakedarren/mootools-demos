65e6fe57dd15493240bea0ef0db3f2f30778aac1
fakedarren <darren.waddell@gmail.com>
2010-12-21 12:31:00 +0000

    Tweaks to Drag.Drop demo

--- demos/Drag.Drop/demo.js
+++ demos/Drag.Drop/demo.js
@@ -1,31 +1,29 @@
 window.addEvent('domready', function(){
 
-	$$('#draggables DIV').each(function(drag){
+	$$('#draggables DIV').makeDraggable({
+			
+		droppables: $$('#droppables DIV'),
 		
-		new Drag.Move(drag, {
-			
-			droppables: $$('#droppables DIV'),
-			
-			onDrop: function(draggable, droppable){
-				if (droppable){
-					draggable.destroy();
-					droppable.morph({
-						'height': droppable.getStyle('height').toInt() + 30,
-						'background-color' : ['#78BA91', '#1D1D20']
-					});
-				} else {
-					draggable.setStyle('background', '#FAEC8F');
-				}
-			},
-			onEnter: function(draggable, droppable){
-				droppable.setStyle('background', '#78BA91');
-			},
-			onLeave: function(draggable, droppable){
-				droppable.setStyle('background', '#1D1D20');
+		onEnter: function(draggable, droppable){
+			droppable.setStyle('background', '#78BA91');
+		},
+		
+		onLeave: function(draggable, droppable){
+			droppable.setStyle('background', '#1D1D20');
+		},
+		
+		onDrop: function(draggable, droppable){
+			if (droppable){
+				draggable.destroy();
+				droppable.morph({
+					'height': droppable.getStyle('height').toInt() + 30,
+					'background-color' : ['#78BA91', '#1D1D20']
+				});
+			} else {
+				draggable.setStyle('background', '#FAEC8F');
 			}
-			
-		});
-	 
+		}
+		
 	});
 
 });
