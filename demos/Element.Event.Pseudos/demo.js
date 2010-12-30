281eb87d51b4706ed68d71a58c8602ec2d1512e2
Arian <arian@aryweb.nl>
2010-12-30 19:46:16 +0100

    Update Element.Event.Pseudos demo

--- demos/Element.Event.Pseudos/demo.js
+++ demos/Element.Event.Pseudos/demo.js
@@ -1,18 +1,25 @@
 
 window.addEvent('domready', function() {
-	// `once` psuedo event provided by mootools-more
-	document.id('clickOnce').addEvent('click:once', function(){
-		alert('you clicked me');
+
+	// `:once` psuedo event provided by mootools-more
+	$('clickOnce').addEvent('click:once', function(event){
+		event.stop();
+
+		this.set('tween', {
+			transition: 'bounce:out',
+			duration: 'long'
+		}).tween('margin-left', 300);
 	});
-	
-	// user defined psuedo event added
-	Event.definePseudo('ctrlKey', function(split, fn, args){
-		if( ! args[0].event.ctrlKey ) return;
-		fn.apply(this, args);
+
+	// we can define our own pseudo events as well, for example to check for the control key
+	Event.definePseudo('ctrl', function(split, fn, args){
+		// args[0] is the Event instance
+		if(args[0].control) fn.apply(this, args);
 	});
-	
+
 	// apply the psuedo event to some elements
-	$$('.item').addEvent( 'click:ctrlKey', function() {
+	$$('.item').addEvent('click:ctrl', function(){
 		this.toggleClass('active');
 	});
+
 });
