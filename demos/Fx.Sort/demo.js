131f254420af9a69a6c8727e03198d5af149fa4c
U-meBeats-laptop\meBeats <meBeats@meBeats-laptop.(none)>
2011-01-02 18:01:51 -0600

    Cleaned up html/js. Added some more styling.

--- demos/Fx.Sort/demo.js
+++ demos/Fx.Sort/demo.js
@@ -11,53 +11,44 @@ window.addEvent('domready', function() {
 	
 	displayDOM();
 
-	$$('.fireSort').addEvent('click',
-		clickHandler(
-			function() {
-				// sort based on text of element that was clicked ( forward, backward, reverse )
-				sorter[this.get('text')]();
-			}
-		)
+	$$('.fireSort').addEvent('click', function(e) {
+            e.preventDefault();
+            //sort based on text of element that was clicked (forward, backward, reverse)
+            sorter[this.get('text')]();
+        }
 	);
 	
-	document.id('order').addEvent('click',
-		clickHandler(
-			function() {
-				alert(sorter.currentOrder);
-			}
-		)
+	document.id('order').addEvent('click', function(e) {
+            e.preventDefault();
+            alert(sorter.currentOrder);
+        }
 	);
 	
-	document.id('reorderDOM').addEvent('click',
-		clickHandler(
-			function() {
-				sorter.rearrangeDOM();
-				displayDOM();
-			}
-		)
+	document.id('reorderDOM').addEvent('click', function(e) {
+            e.preventDefault();
+            sorter.rearrangeDOM();
+            displayDOM();
+        }
 	);
 	
 	document.id('mode').addEvent('change', function() {
-		sorter.rearrangeDOM(); // rearrange DOM first so that elements stack properly
-		sorter.options.mode = (this.checked) ? "vertical":"horizontal";
-		container.toggleClass('container');
+        sorter.rearrangeDOM(); //rearrange DOM first so that elements stack properly
+        sorter.options.mode = (this.checked) ? "vertical":"horizontal";
+        container.toggleClass('container');
+        displayDOM();
 	});
 	
-	document.id('swap').addEvent('click', 
-		clickHandler(
-			function() {
-				var elems = container.getChildren();
-				sorter.swap(elems[0], elems[elems.length-1]);
-			}
-		)
+	document.id('swap').addEvent('click', function(e) {
+            e.preventDefault();
+            var elems = container.getChildren();
+            sorter.swap(elems[0], elems[elems.length-1]);
+        }
 	);
 	
-	document.id('sort').addEvent('click',
-		clickHandler(
-			function(e) {
-				sorter.sort([1,3,0,2,4]);
-			}
-		)
+	document.id('sort').addEvent('click', function(e) {
+            e.preventDefault();
+            sorter.sort([1,3,0,2,4]);
+        }
 	);
 	
 	document.id('fxTransition').addEvent('change', function() {
@@ -72,21 +63,12 @@ window.addEvent('domready', function() {
 		sorter.options.duration = this.value.toInt();
 	});
 	
-	// helper function, displays the DOM visually to see how the sort effects it
+	//helper function, displays the DOM visually to see how the sort effects it
 	function displayDOM() {
-		var str = '';
-		container.getChildren().each( function(item) {
-			str += '<div style="'+item.style.cssText+'">'+item.get('text')+'</div>\r\n';
-		});
-		document.id('output').set('text',  str);
-	}
-	
-	// partial function, helper so that preventDefault is automatically called on clicks
-	function clickHandler(fn) {
-		var args = arguments;
-		return function(e) {
-			e.preventDefault();
-			fn.apply(this, args);
-		};
+        var str = '';
+        container.getChildren().each( function(item) {
+            str += '<div style="'+item.style.cssText+'">'+item.get('text')+'</div>\r\n';
+        });
+        document.id('output').set('text',  str);
 	}
 });
